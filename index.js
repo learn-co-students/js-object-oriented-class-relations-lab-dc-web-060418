09fd9a11e9e32656ef8f71c3aca2b9175261a2a7
Essa Hijazi <github email address>
2018-07-28 13:59:39 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,73 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let driverId = 0;
+class Driver {
+    constructor(name) {
+        this.id = ++driverId;
+        this.name = name;
+
+        store.drivers.push(this);
+    }
+
+    passengers() {
+        return this.trips().map(trip =>
+            trip.passenger()
+        )
+    }
+
+    trips() {
+        return store.trips.filter(trip =>
+            trip.driverId === this.id
+        )
+    }
+}
+
+
+
+let passengerId = 0;
+class Passenger {
+    constructor(name){
+        this.id = ++passengerId;
+        this.name = name;
+
+        store.passengers.push(this)
+    }
+
+    drivers() {
+        return this.trips().map(trip => 
+            trip.driver()
+        )
+    }
+
+    trips() {
+        return store.trips.filter(trip => 
+            trip.passengerId === this.id
+        )
+    }
+
+}
+
+
+
+let tripId = 0;
+class Trip{
+    constructor(driver, passenger){
+        this.id = ++tripId;
+        this.driverId = driver.id;
+        this.passengerId = passenger.id;
+
+        store.trips.push(this);
+    }
+
+    passenger() {
+        return store.passengers.find(passenger =>
+            passenger.id === this.passengerId
+        )
+    }
+
+    driver() {
+        return store.drivers.find(driver =>
+            driver.id === this.driverId
+        )
+    }
+}
